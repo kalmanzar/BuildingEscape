a4c8d4884233f302b7cf2ece78761e65cf06374b
Kevin Almanzar <kevinalmanzar8@gmail.com>
2017-07-25 23:20:30 -0400

    Trigger Volume

--- Source/BuildingEscape/OpenDoor.h
+++ Source/BuildingEscape/OpenDoor.h
@@ -19,8 +19,14 @@ public:
 	virtual void BeginPlay() override;
 	
 	// Called every frame
-	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+    virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
 
+private:
+    UPROPERTY(VisibleAnywhere)
+    float OpenAngle = 90.f;
+    
+    UPROPERTY(EditAnywhere)
+    ATriggerVolume* PressurePlate;
 		
 	
 };
